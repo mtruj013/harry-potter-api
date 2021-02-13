701494b7a64a3d3d1fd4de65288be09ef241920c
mtruj013 <mtruj013@fiu.edu>
2021-02-13 23:15:25 +0100

    added cleaner seed

---
+++ data/seeds/000_cleaner.js
@@ -0,0 +1,9 @@
+const cleaner = require('knex-cleaner');
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return cleaner.clean(knex, {
+    mode: "delete", // reset ids
+    ignoreTables: ['knex_migrations', 'knex_migrations_lock'] // don't empty migrations tables
+  })
+};
