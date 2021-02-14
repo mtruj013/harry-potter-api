08d2fc005b4164c849e4f13fb8d59de3b09054ae
mtruj013 <mtruj013@fiu.edu>
2021-02-14 23:28:11 +0100

    added testing configuration

--- knexfile.js
+++ knexfile.js
@@ -37,20 +37,16 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+  testing: {
+    client: "pg",
+    connection: {
+      filname: ":memory:"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
