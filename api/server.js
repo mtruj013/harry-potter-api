7fa943f825fb5ebc535c14da61878b7d99a79c4c
mtruj013 <mtruj013@fiu.edu>
2021-02-16 03:43:50 +0100

    updated with router information

--- api/server.js
+++ api/server.js
@@ -3,7 +3,11 @@ const helmet = require("helmet");
 
 const server = express();
 
+const SchoolsRouter = require("../schools/schoolsRouter.js")
+
 server.use(helmet());
 server.use(express.json());
+
+server.use("/api/schools", SchoolsRouter)
 
 module.exports = server;
