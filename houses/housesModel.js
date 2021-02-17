e3963bb22e5095eca394e124d9371900a6236d4d
mtruj013 <mtruj013@fiu.edu>
2021-02-17 23:36:53 +0100

    added house model and endpoints

---
+++ houses/housesModel.js
@@ -0,0 +1,47 @@
+const db = require("../data/dbConfig");
+
+module.exports = {
+    findAllHouses,
+    findHouseById,
+    findHouseByName,
+    addHouse,
+    updateHouse,
+    deleteHouse
+}
+
+function findAllHouses(){
+    return db("houses")
+}
+
+function findHouseById(id){
+    return db("houses")
+        .where("id", id)
+        .first()
+}
+
+function findHouseByName(house) {
+    return db("houses")
+        .select("*")
+        .where("house_name", "ilike", `%${house}`)
+        .first()
+}
+
+function addHouse(house, school_id) {
+    return db("houses")
+        .insert(house, school_id)
+}
+
+function updateHouse(changes, id) {
+    return db("houses")
+        .where({id}, id)
+        .update(changes)
+        .then(ids => {
+            return findHouseById(id[0])
+        })
+}
+
+function deleteHouse(id) {
+    return db("houses")
+        .where({id})
+        .del()
+}
